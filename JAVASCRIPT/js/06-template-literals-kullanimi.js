f684052a424cb8b6188d0cd0a5807e14165d8258
Uygar Süslü <uygarsuslu1@gmail.com>
2022-02-09 22:29:36 +0300

    Merge branch 'main' of https://github.com/uygarsuslu/Frontend-Web-Development-Patikasi

--- JAVASCRIPT/js/06-template-literals-kullanimi.js
+++ JAVASCRIPT/js/06-template-literals-kullanimi.js
@@ -3,7 +3,7 @@
 let username = "uygar"
 let DOMAIN = "kodluyoruz.org"
 
-let email = username + "@" + DOMAIN 
+let email = username + "@" + DOMAIN
 //console.log("Merhaba", username, "sitemize hoşgeldin", "mail adresin: ", email)
 
 let info = `
@@ -11,7 +11,32 @@ Merhaba ${username} sitemize hoşgeldin..
 mail adresin: ${email}
 kısa isminiz: ${username[0]}.
 mail adresinin uzunluğu: ${email.length} karakter
-borcunuz: ${( 2 + 3 ) * 5} TL
+borcunuz: ${(2 + 3) * 5} TL
 gunun saat bilgisi: ${new Date().getHours()}
 `
 console.log(info)
+
+const kitap = {
+    ad: "Fırtına",
+    yazar: "Shakespeare",
+    tarih: 1610
+}
+const bookTable = `
+<table border>
+    <tbody>
+        <tr>
+            <td>Kitap</td>
+            <td>${kitap.ad}</td>
+        </tr>
+        <tr>
+            <td>Yazar</td>
+            <td>${kitap.yazar}</td>
+        </tr>
+        <tr>
+            <td>Tarih</td>
+            <td>${kitap.tarih}</td>
+        </tr>
+    </tbody>
+</table>
+`
+document.body.innerHTML = bookTable
