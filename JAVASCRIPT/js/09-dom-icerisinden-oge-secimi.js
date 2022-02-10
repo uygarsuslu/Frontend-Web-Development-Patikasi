2b359e6e58ff276dc2d1144859564f803c0347cc
Uygar Süslü <uygarsuslu1@gmail.com>
2022-02-10 12:10:02 +0300

    dom icerisinden oge secimi added

--- JAVASCRIPT/js/09-dom-icerisinden-oge-secimi.js
+++ JAVASCRIPT/js/09-dom-icerisinden-oge-secimi.js
@@ -3,10 +3,11 @@
 // let h2 = document.getElementsByTagName('h2')
 
 let title = document.getElementById("title")
-title.innerHTML = "Degisen Bilgi"
+title.innerHTML = "Degisen Bilgi" // bilgiyi degistirme islemi //
 console.log(title.innerHTML)
 
 let link = document.querySelector("#kodluyoruzLink")
-link.innerHTML += " degisti"
+link.innerHTML += " degisti" // bilgiyi degistirme islemi //
 link.style.color = "red"
 link.classList.add("btn")
+console.log(link.innerHTML)
