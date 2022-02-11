eded1a3b786d004f90871c2c8d80c6f3885310cb
Uygar Süslü <uygarsuslu1@gmail.com>
2022-02-11 14:13:32 +0300

    fonksiyon nedir added

---
+++ JAVASCRIPT/js/18-fonksiyon-nedir-neden-kullanilir.js
@@ -0,0 +1,26 @@
+// ********** fonksiyon nedir neden kullanilir ********** //
+
+function helloWorld() {
+    console.log("Hello World")
+}
+
+function merhabaDunya() {
+    console.log("Merhaba Dünya")
+    helloWorld()
+}
+
+merhabaDunya() // BUNU YAZMAZSAK FONKSİYON ÇALIŞMAZ //
+
+// ***** HATA ALIRIZ ***** //
+// function userCheck() {
+//     if (userName && age >= 18) {
+//         info.innerHTML = "ehliyet alabilirsiniz"
+//     }
+//     else if (!userName) {
+//         info.innerHTML = "Kullanici Adiniz Yok"
+//     }
+//     else if (!(age >= 18)) {
+//         info.innerHTML = "Yas Bilginiz Yok veya 18 Yasindan Kucuksunuz"
+//     }
+// }
+
