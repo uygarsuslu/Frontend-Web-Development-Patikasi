fe456502eeb1eb96b2466425efd3a96aa1831627
Uygar Süslü <uygarsuslu1@gmail.com>
2022-02-11 22:53:48 +0300

    fonksiyona parametre ve geridonus eklemek added

--- JAVASCRIPT/js/19-fonksiyona-parametre-(params)-ve-geridonus-(return)-eklemek.js
+++ JAVASCRIPT/js/19-fonksiyona-parametre-(params)-ve-geridonus-(return)-eklemek.js
@@ -41,3 +41,47 @@ let htmlInfo = `<span style="color:red">Color REDDD</span>`
 domIdWriteInfo("info", greetings2("Yönetim", "Bilişim") )
 domIdWriteInfo("greeting", htmlInfo )
 // domIdWriteInfo("greeting", greetings2("Ipsum", "Lorem") )
+
+/* ********************************************************************************************* */
+
+// Herhangi bir yarıçapa sahip bir dairenin alanını hesaplayan fonksiyonu yazınız.
+
+const PI = 3.14; // Formülde kullandığımız sabit sayı pi'yi bu şekilde alabiliriz.
+function daireAlaniHesaplama (r) // Fonksiyonumuz, r parametresini alıyor.
+{
+    var islemSonucu = PI*r*r; // Dairenin alanını hesaplayacak işlemimiz.
+    return islemSonucu;		 // return ifadesiyle sonucu dönüyoruz.
+}
+
+var donenSonuc = daireAlaniHesaplama(3); // Argüman olarak, alanını hesaplamak istediğimiz herhangi bir dairenin yarıçapı için 3 rakamı verildi. 
+console.log(donenSonuc); // 28.25999999 sonucunu göreceğiz.
+
+/* ********************************************************************************************* */
+
+function daireAlaniHesaplama (r, PI = 3.14) 
+{ 
+    return PI*r*r;		
+}
+var donenSonuc = daireAlaniHesaplama(3); 
+console.log(donenSonuc); // 28.25999999 sonucunu göreceğiz.
+
+// Farklı yarıçaplardaki iki farklı dairenin, alanları çarpımını veren bir fonksiyon yazınız.
+
+/* ********************************************************************************************* */
+
+function daireAlaniHesaplama (r, PI = 3.14) 
+{ 
+    return PI*r*r;		
+}
+
+function carpma (a1, a2)
+{
+    return a1*a2;
+}
+
+var alan1 = daireAlaniHesaplama(5); // 5 ve 6 argümanları için dairelerin alanları hesaplandı.
+var alan2 = daireAlaniHesaplama(6); 
+var donenSonuc = carpma(alan1, alan2); // Hesaplanan alanlar argüman olarak verildi.
+console.log(donenSonuc);  			  // 354.9456 olarak hesaplandı.
+
+// Bu örnekten de görüldüğü gibi, bir fonksiyonu çağırdıktan sonra elde ettiğimiz bir değer ile ikinci bir fonksiyonun argümanı olarak kullanabiliyoruz. Bunu yapabilmemize olanak sağlayan şey, değer döndüren(return) fonksiyonları kullanmamız olduğunu söyleyebiliriz.
