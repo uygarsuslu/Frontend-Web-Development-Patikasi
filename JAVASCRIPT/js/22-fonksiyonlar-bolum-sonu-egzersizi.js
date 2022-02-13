1b1c52f124b536b25384faaa2fd42d4c8163b04d
Uygar Süslü <uygarsuslu1@gmail.com>
2022-02-13 19:29:07 +0300

    fonksiyonlar bolum sonu egzersizi added

---
+++ JAVASCRIPT/js/22-fonksiyonlar-bolum-sonu-egzersizi.js
@@ -0,0 +1,17 @@
+// ********** fonksiyonlar bolum sonu egzersizi ********** //
+
+let counter = 0
+let counterDOM = document.querySelector(`#counter`) 
+let increaseDOM = document.querySelector(`#increase`)
+let decreaseDOM = document.querySelector(`#decrease`)
+
+counterDOM.innerHTML = counter
+
+increaseDOM.addEventListener("click",clickEvent)
+decreaseDOM.addEventListener("click", clickEvent)
+
+function clickEvent() {
+    console.log(this.id)
+    this.id == "increase" ? counter += 1 : counter -= 1 // counterDOM.innerHTML = counter'dı kısalttık //
+    counterDOM.innerHTML = counter
+}
