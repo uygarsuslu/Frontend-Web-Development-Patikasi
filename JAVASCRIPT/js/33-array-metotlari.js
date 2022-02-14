bec8107987c163508d3daf12f314417d7cf2a16a
Uygar Süslü <uygarsuslu1@gmail.com>
2022-02-14 23:04:01 +0300

    tum array metodlari added

--- JAVASCRIPT/js/33-array-metotlari.js
+++ JAVASCRIPT/js/33-array-metotlari.js
@@ -59,3 +59,68 @@ console.log(allUsers.join(" --- "))
 allUsers.splice(allUsers.length-1, 0, "Melisa")
 allUsers.splice(Math.floor( allUsers.length / 2 ), 0, "Lorem")
 console.log(allUsers)
+
+// Bir dizideki sayıların 5 katından oluşan başka bir dizi oluşturalım //
+const sayilar = [1,2,3]
+let sayilarin5kati = sayilar.map(function(sayi) {
+    return sayi*5
+});
+console.log("map: ", sayilarin5kati) // Çıktı olarak [5,10,15] görmeyi bekleriz.
+
+// SOME METOT KULLANİMİ //
+const dayilar = [4,5,6];
+// sonucu görebilmek çıktıyı sonuc1 adlı değişkende tutalım:
+const sonucccc = dayilar.some(function(sayi) {
+    return sayi > 5 ;
+});
+console.log("some: ", sonucccc);
+
+// EVERY METOT KULLANİMİ //
+// belirtilen bir koşulun dizideki tüm elemanlara uyup uymadığını kontrol eder //
+const ayilar = [4,5,6];
+const sonuc1 = ayilar.every(function(sayi) {
+    return sayi > 3;
+});
+console.log("every: ", sonuc1);
+
+// FİLTER METOT KULLANİMİ //
+const kayilar = [1,2,3,4,5];
+const filtrelenmisSayilar = kayilar.filter(function(sayi) {
+    return sayi >3;
+});
+// Orijinal diziyi 3'ten büyük olan sayılar için filtrelediğimizde yeni oluşacak dizi [4,5] olacaktır.
+console.log("filter: ", filtrelenmisSayilar);
+
+// FİND METOT KULLANİMİ //
+const tayilar = [4,5,6,7];
+const bulunacakEleman1 = tayilar.find(function(sayi) {
+    return sayi > 5;
+});
+// Dizi içerisinde 5'ten büyük birden fazla eleman olduğu için koşula uyan ilk elemanı yani 6'yı görmeyi bekleriz.
+console.log("find: ", bulunacakEleman1);
+
+// SORT METOT KULLANİMİ //
+const cayilar = [3,5,2,10,4];
+cayilar.sort();
+console.log(cayilar); // Çıktı olarak [10, 2, 3, 4, 5] alırız. 
+//(Sayıların string halleri UTF-16 değerlerine göre sıralandıkları için)
+
+const hayilar = [3,5,2,10,4];
+hayilar.sort(function(a,b) {
+    return b-a;
+});
+console.log(hayilar); // Çıktı olarak [10, 5, 4, 3, 2] alırız.
+
+
+// REDUCE METOT KULLANİMİ //
+const jayilar = [10,20,30];
+// Dizi içindeki sayıları toplayarak indirgeyecek bir fonksiyon yazalım:
+function indirgeyici (akumulator, sayi) {
+    return akumulator + sayi;
+}
+// Bu fonksiyonu ve toplamaya 0'dan başlayacağımızı belirten 0 sayısını metodumuza parametre olarak girelim ve sonucu bir değişkende tutalım:
+const sonuc5 = jayilar.reduce(indirgeyici,0);
+console.log("reduce1: ", sonuc5); //0 + 10 + 20 + 30 = 60 olacağından çıktı olarak 60 bekleriz.
+// Eğer akümülatorümüzü 0 yerine 5'den başlatsaydık çıkabilecek sonucu görelim:
+const sonuc6 = jayilar.reduce(indirgeyici, 5);
+console.log("reduce2: ", sonuc6); // 5 + 10 + 20 + 30 = 65 olacağından çıktı olarak 65 bekleriz.
