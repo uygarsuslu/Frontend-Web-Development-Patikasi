04eb7fb8519fd277dee9154648915792d3dec0b6
Uygar Süslü <uygarsuslu1@gmail.com>
2022-02-15 11:55:16 +0300

    while dongusu added

---
+++ JAVASCRIPT/js/37-while-dongusu.js
@@ -0,0 +1,19 @@
+// ********** while dongusu ********** // // bir şey olana kadar //
+
+// let counter = 0
+// while (counter < 10) {
+//     console.log(counter)
+//     counter ++
+// }
+
+// let userName = ""
+// while (!userName) {
+//     userName = prompt("Kullanici Adini Giriniz: ")
+//     console.log(userName)
+// }
+
+var degisken = 1
+while (degisken <= 10) {
+    console.log("degiskenin degeri: " + degisken)
+    degisken ++ // dongu sonsuz olmamasi icin degiskeni arttiriyoruz //
+}
